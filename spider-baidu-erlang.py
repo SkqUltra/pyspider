98846d9d18cfa578a4c428369c9aabe254fba196
Skq Ultra <531671505@qq.com>
2017-09-24 11:21:46 +0800

    tieba erlang
    爬去百度erlang吧所有帖子标题

--- spider-baidu-erlang.py
+++ spider-baidu-erlang.py
@@ -1,24 +1,37 @@
-# encoding: UTF-8
+# -*- coding: UTF-8 -*-
 
 from bs4 import BeautifulSoup
 import requests
+import os
+import codecs
 
-url = "http://tieba.baidu.com/f?kw=erlang&ie=utf-8&pn="
-
-beginpage = 0
-endpage = 5
+url = "//tieba.baidu.com/f?kw=erlang&ie=utf-8&pn=0"
+saveurl = "/baidu-erlang.txt"
 
 class Spider:
 
 	def __init__(self, url):
 		self.url = url
+		self.saveurl = saveurl
+		self.data = []
 
-	def spider(self, page):
-		data = requests.get(self.url+str(page)).content
-		soup = BeautifulSoup(data, "html.parser")
-		# soup.find('div', attrs = {'class': "threadlist_title pull_left j_th_tit "}).find('a').get_text()
+	def spider(self):
+		html = requests.get("http:"+self.url).content
+		soup = BeautifulSoup(html, "lxml")
 		for li in soup.find_all('div', attrs = {'class': "threadlist_title pull_left j_th_tit "}):
-		 	print li.find('a').get_text()
+		 	self.data.append(li.find('a').get_text()) 
+		nexturl = soup.find('div', attrs = {'class':"pagination-default clearfix"}).find('a', attrs = {'class':"next pagination-item "})
+		if nexturl != None:
+			self.url = nexturl.get('href')
+			self.spider()
+
+
+	def save(self):
+		f = codecs.open(os.getcwd()+saveurl,'w+',"utf-8") 
+		f.write("\n".join(self.data))  
+		f.close()  
+
 
 sp = Spider(url)
-sp.spider(0)
+sp.spider()
+sp.save()
